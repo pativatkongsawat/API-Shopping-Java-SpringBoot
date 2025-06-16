b9107ce4d75dae93ba11eaa69286465ec10e9fda
pativatkongsawat <pativat.k@kkumail.com>
2025-06-16 16:35:19 +0700

    [ADD] User Model

--- src/main/java/com/example/spring/model/UserModel.java
+++ src/main/java/com/example/spring/model/UserModel.java
@@ -2,13 +2,45 @@ package com.example.spring.model;
 
 
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class UserModel {
 
-   
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+
+    private String address;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+
+
+    @Column(name = "permission_id")
+    private int permissionId;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
     
 }

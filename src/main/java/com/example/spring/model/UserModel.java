f6e373502e342602ecaee87070c06a71f64599e8
pativatkongsawat <pativat.k@kkumail.com>
2025-06-16 16:27:10 +0700

    java Spring boots Shopping

---
+++ src/main/java/com/example/spring/model/UserModel.java
@@ -0,0 +1,14 @@
+package com.example.spring.model;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserModel {
+
+   
+    
+}

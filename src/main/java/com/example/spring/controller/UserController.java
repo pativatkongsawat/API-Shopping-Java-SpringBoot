93b0adcc5b98cb79d710e5d04f4c9f0ac06baf9b
pativatkongsawat <pativat.k@kkumail.com>
2025-06-17 13:38:40 +0700

    [ADD] Handler

--- src/main/java/com/example/spring/controller/UserController.java
+++ src/main/java/com/example/spring/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.example.spring.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.spring.model.UserModel;
@@ -23,6 +30,17 @@ public class UserController {
     @GetMapping
     public List<UserModel> getAllUser(){
         return userService.GetAllUser();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserModel> getUserbyId(@PathVariable UUID id){
+        Optional <UserModel> user = userService.GetById(id);
+        return user.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+        
+    }
+
+    @PostMapping
+    public ResponseEntity<UserModel> createuser(@RequestBody UserModel user){
+        return ResponseEntity.ok(userService.CreateUser(user));
     }
 
     
---
+++ src/main/java/com/example/spring/handler/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.example.spring.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleBadRequest(IllegalArgumentException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", 400);
+        body.put("error", "Bad Request");
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleInternalServerError(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", 500);
+        body.put("error", "Internal Server Error");
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}

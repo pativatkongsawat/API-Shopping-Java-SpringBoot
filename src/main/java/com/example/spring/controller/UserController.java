988e47a81b2f436aa4f6cdd4c77ad4e329063405
pativatkongsawat <pativat.k@kkumail.com>
2025-06-17 13:04:08 +0700

    [FIX] Pom.xml

--- src/main/java/com/example/spring/controller/UserController.java
+++ src/main/java/com/example/spring/controller/UserController.java
@@ -1,5 +1,29 @@
 package com.example.spring.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring.model.UserModel;
+
+import com.example.spring.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+    
+    private final UserService userService;
+
+    public UserController (UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<UserModel> getAllUser(){
+        return userService.GetAllUser();
+    }
+
     
 }

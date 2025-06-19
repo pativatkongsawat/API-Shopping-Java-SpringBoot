0e597bb1629f6bcf6fbbde42df1f0479664a1446
pativatkongsawat <pativat.k@kkumail.com>
2025-06-19 19:29:07 +0700

    [EDIT] Validation Create Product

--- src/main/java/com/example/spring/service/ProductService.java
+++ src/main/java/com/example/spring/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.example.spring.service;
 
+import java.time.LocalDateTime;
+
 import com.example.spring.model.ProductModel;
 import com.example.spring.repository.ProductRepository;
 
@@ -14,6 +16,21 @@ public class ProductService {
 
 
     public ProductModel createProduct(ProductModel product){
+
+        if (product.getName() == null || product.getName().isBlank() ||
+            product.getDescription() == null || product.getDescription().isBlank() ||
+            product.getPrice() == 0 || product.getQuantity() == 0 ||
+            product.getImage() == null || product.getImage().isBlank() ||
+            product.getCategoryId() == 0
+        ) {
+            throw new IllegalArgumentException("กรุณากรอกข้อมูลให้ครบทุกช่อง");
+        }
+
+        product.setCreatedAt(LocalDateTime.now());
+        product.setUpdatedAt(LocalDateTime.now());
+        product.setDeletedAt(null);
+
+
         return productRepository.save(product);
     }
     

ec4d47fb909c34ba806d4c630fb9a7dc88a7f417
pativatkongsawat <pativat.k@kkumail.com>
2025-06-23 14:42:12 +0700

    [ADD] Model orders

--- src/main/java/com/example/spring/model/OrderModel.java
+++ src/main/java/com/example/spring/model/OrderModel.java
@@ -1,8 +1,68 @@
 package com.example.spring.model;
 
+
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "orders")
 public class OrderModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    private LocalDateTime create_at;
+
+
+    private LocalDateTime updated_at;
+
+
+    private LocalDateTime deleted_at;
+
+    private String user_id;
+
+
+    private Double total_price;
+
+
+    private String created_by;
+
+    private String status;
+
+    public OrderModel(){};
+
+
+    public Integer getId() {return id;}
+    public void setId(Integer id) {this.id = id;}
+
+    public LocalDateTime getCreateAt() {return create_at;}
+    public void setCreateAt(LocalDateTime create_at) {this.create_at = create_at;}
+
+    public LocalDateTime getUpdateAt() {return updated_at;}
+    public void setUpdateAt(LocalDateTime updated_at) {this.updated_at = updated_at;}
+
+    public LocalDateTime getDeleteAt(){return deleted_at;}
+    public void setDeleteAt(LocalDateTime deleted_at){this.deleted_at = deleted_at;}
+
+    public String getUserId(){return user_id;}
+    public void setUserId(String user_id) {this.user_id = user_id;}
+
+    public Double getTotalPrice(){return total_price;}
+    public void setTotalPrice(Double total_price) {this.total_price = total_price;}
+
+    public String getCreateBy(){return created_by;}
+    public void setCreateBy(String created_by ) {this.created_by = created_by;}
+
+    public String getStatus(){return status;}
+    public void setStatus(String status) {this.status = status;}
+
+
     
 }

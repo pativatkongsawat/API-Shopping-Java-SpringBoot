fb88f767ec18f9b74c2e99181ad910a3469224f0
pativatkongsawat <pativat.k@kkumail.com>
2025-06-23 19:24:12 +0700

    [FIX] Controller Order

--- src/main/java/com/example/spring/controller/OrderController.java
+++ src/main/java/com/example/spring/controller/OrderController.java
@@ -1,13 +1,16 @@
 package com.example.spring.controller;
 
-import java.time.LocalDateTime;
+
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.spring.helper.OrderCreateRequest;
-import com.example.spring.helper.RequestProduct;
+
 import com.example.spring.model.OrderHasProductModel;
 import com.example.spring.service.OrderService;
 
@@ -17,9 +20,14 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    public OrderController(OrderService orderService){
+    public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
- 
+    @PostMapping("/orders")
+    public ResponseEntity<List<OrderHasProductModel>> createOrder(@RequestBody OrderCreateRequest order) {
+        List<OrderHasProductModel> result = orderService.CreateOrder(order);
+        return ResponseEntity.ok(result);
+    }
+
 }

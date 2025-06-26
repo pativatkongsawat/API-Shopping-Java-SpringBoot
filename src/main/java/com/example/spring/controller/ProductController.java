80d0ac6f727334a26ae0464791fea56039e0aa79
pativatkongsawat <pativat.k@kkumail.com>
2025-06-26 15:45:40 +0700

    [ADD] Path TO Api Products

--- src/main/java/com/example/spring/controller/ProductController.java
+++ src/main/java/com/example/spring/controller/ProductController.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,7 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @PostMapping("/")
     public ResponseEntity<ProductModel> CreateProduct(@RequestBody ProductModel product){
 
         ProductModel data = productService.createProduct(product);
@@ -31,6 +35,7 @@ public class ProductController {
         return ResponseEntity.ok(data);
     }
 
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> DeleteProduct(@PathVariable int id){
 
         if(productService.deleteProduct(id)){
@@ -42,10 +47,12 @@ public class ProductController {
 
     }
 
+    @GetMapping("/")
     public List<ProductModel> getAllProduct(){
         return productService.getAllProduct();
     }
 
+    @GetMapping("/{id}")
     public ResponseEntity<ProductModel> getProductById(@PathVariable int id){
 
         Optional<ProductModel> product = productService.getProductById(id);
